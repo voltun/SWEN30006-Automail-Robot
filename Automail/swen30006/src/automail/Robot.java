13f808e3faceb8687183f35b6f60d3812bc762c5
Meric Ungor <mungor@student.unimelb.edu.au>
2019-09-13 01:24:14 +1000

    Replace Robot.java

--- Automail/swen30006/src/automail/Robot.java
+++ Automail/swen30006/src/automail/Robot.java
@@ -17,6 +17,7 @@ public class Robot {
     protected final String id;
     /** Possible states the robot can be in */
     public enum RobotState { DELIVERING, WAITING, RETURNING }
+    public enum ModeType {OVERDRIVE, NORMAL}
     public RobotState current_state;
     private int current_floor;
     private int destination_floor;
@@ -27,6 +28,7 @@ public class Robot {
     private MailItem tube = null;
     
     private int deliveryCounter;
+    public Mode mode;
     
 
     /**
@@ -45,6 +47,19 @@ public class Robot {
         this.mailPool = mailPool;
         this.receivedDispatch = false;
         this.deliveryCounter = 0;
+        this.mode = new Normal();
+    }
+    
+    public Robot(IMailDelivery delivery, IMailPool mailPool, Mode mode){
+    	id = "R" + hashCode();
+        // current_state = RobotState.WAITING;
+    	current_state = RobotState.RETURNING;
+        current_floor = Building.MAILROOM_LOCATION;
+        this.delivery = delivery;
+        this.mailPool = mailPool;
+        this.receivedDispatch = false;
+        this.deliveryCounter = 0;
+        this.mode = mode;
     }
     
     public void dispatch() {
@@ -55,61 +70,139 @@ public class Robot {
      * This is called on every time step
      * @throws ExcessiveDeliveryException if robot delivers more than the capacity of the tube without refilling
      */
-    public void step() throws ExcessiveDeliveryException {    	
-    	switch(current_state) {
-    		/** This state is triggered when the robot is returning to the mailroom after a delivery */
-    		case RETURNING:
-    			/** If its current position is at the mailroom, then the robot should change state */
-                if(current_floor == Building.MAILROOM_LOCATION){
-                	if (tube != null) {
-                		mailPool.addToPool(tube);
-                        System.out.printf("T: %3d > old addToPool [%s]%n", Clock.Time(), tube.toString());
-                        tube = null;
-                	}
-        			/** Tell the sorter the robot is ready */
-        			mailPool.registerWaiting(this);
-                	changeState(RobotState.WAITING);
-                } else {
-                	/** If the robot is not at the mailroom floor yet, then move towards it! */
-                    moveTowards(Building.MAILROOM_LOCATION);
-                	break;
+//    public void step() throws ExcessiveDeliveryException {    	
+//    	switch(current_state) {
+//    		/** This state is triggered when the robot is returning to the mailroom after a delivery */
+//    		case RETURNING:
+//    			/** If its current position is at the mailroom, then the robot should change state */
+//                if(current_floor == Building.MAILROOM_LOCATION){
+//                	if (tube != null) {
+//                		mailPool.addToPool(tube);
+//                        System.out.printf("T: %3d > old addToPool [%s]%n", Clock.Time(), tube.toString());
+//                        tube = null;
+//                	}
+//        			/** Tell the sorter the robot is ready */
+//        			mailPool.registerWaiting(this);
+//                	changeState(RobotState.WAITING);
+//                } else {
+//                	/** If the robot is not at the mailroom floor yet, then move towards it! */
+//                    moveTowards(Building.MAILROOM_LOCATION);
+//                	break;
+//                }
+//    		case WAITING:
+//                /** If the StorageTube is ready and the Robot is waiting in the mailroom then start the delivery */
+//                if(!isEmpty() && receivedDispatch){
+//                	receivedDispatch = false;
+//                	deliveryCounter = 0; // reset delivery counter
+//        			setRoute();
+//                	changeState(RobotState.DELIVERING);
+//                }
+//                break;
+//    		case DELIVERING:
+//    			if(current_floor == destination_floor){ // If already here drop off either way
+//                    /** Delivery complete, report this to the simulator! */
+//                    delivery.deliver(deliveryItem);
+//                    deliveryItem = null;
+//                    deliveryCounter++;
+//                    if(deliveryCounter > 2){  // Implies a simulation bug
+//                    	throw new ExcessiveDeliveryException();
+//                    }
+//                    /** Check if want to return, i.e. if there is no item in the tube*/
+//                    if(tube == null){
+//                    	changeState(RobotState.RETURNING);
+//                    }
+//                    else{
+//                        /** If there is another item, set the robot's route to the location to deliver the item */
+//                        deliveryItem = tube;
+//                        tube = null;
+//                        setRoute();
+//                        changeState(RobotState.DELIVERING);
+//                    }
+//    			} else {
+//	        		/** The robot is not at the destination yet, move towards it! */
+//	                moveTowards(destination_floor);
+//    			}
+//                break;
+//    	}
+//    }
+    
+    
+    
+  public void step() throws ExcessiveDeliveryException {    	
+	
+	  switch(current_state) {
+		/** This state is triggered when the robot is returning to the mailroom after a delivery */
+		case RETURNING:
+			/** If its current position is at the mailroom, then the robot should change state */
+            if(current_floor == Building.MAILROOM_LOCATION){
+            	if (tube != null) {
+            		mailPool.addToPool(tube);
+                    System.out.printf("T: %3d > old addToPool [%s]%n", Clock.Time(), tube.toString());
+                    tube = null;
+            	}
+    			/** Tell the sorter the robot is ready */
+    			mailPool.registerWaiting(this);
+            	changeState(RobotState.WAITING);
+            } else {
+            	/** If the robot is not at the mailroom floor yet, then move towards it! */
+                current_floor = mode.update(current_floor, Building.MAILROOM_LOCATION, false, this);
+            	break;
+            }
+		case WAITING:
+            /** If the StorageTube is ready and the Robot is waiting in the mailroom then start the delivery */
+            if(!isEmpty() && receivedDispatch){
+            	receivedDispatch = false;
+            	deliveryCounter = 0; // reset delivery counter
+    			setRoute();
+            	changeState(RobotState.DELIVERING);
+            }
+            break;
+		case DELIVERING:
+			if(current_floor == destination_floor){ // If already here drop off either way
+                /** Delivery complete, report this to the simulator! */
+				mailDelivered(mode instanceof Overdrive, deliveryItem.weight);
+                delivery.deliver(deliveryItem);
+                deliveryItem = null;
+                deliveryCounter++;
+                if(deliveryCounter > 2){  // Implies a simulation bug
+                	throw new ExcessiveDeliveryException();
                 }
-    		case WAITING:
-                /** If the StorageTube is ready and the Robot is waiting in the mailroom then start the delivery */
-                if(!isEmpty() && receivedDispatch){
-                	receivedDispatch = false;
-                	deliveryCounter = 0; // reset delivery counter
-        			setRoute();
-                	changeState(RobotState.DELIVERING);
+                /** Check if want to return, i.e. if there is no item in the tube*/
+                if(tube == null){
+                	changeState(RobotState.RETURNING);
                 }
-                break;
-    		case DELIVERING:
-    			if(current_floor == destination_floor){ // If already here drop off either way
-                    /** Delivery complete, report this to the simulator! */
-                    delivery.deliver(deliveryItem);
-                    deliveryItem = null;
-                    deliveryCounter++;
-                    if(deliveryCounter > 2){  // Implies a simulation bug
-                    	throw new ExcessiveDeliveryException();
-                    }
-                    /** Check if want to return, i.e. if there is no item in the tube*/
-                    if(tube == null){
-                    	changeState(RobotState.RETURNING);
-                    }
-                    else{
-                        /** If there is another item, set the robot's route to the location to deliver the item */
-                        deliveryItem = tube;
-                        tube = null;
-                        setRoute();
-                        changeState(RobotState.DELIVERING);
-                    }
-    			} else {
-	        		/** The robot is not at the destination yet, move towards it! */
-	                moveTowards(destination_floor);
-    			}
-                break;
-    	}
+                else{
+                    /** If there is another item, set the robot's route to the location to deliver the item */
+                    deliveryItem = tube;
+                    tube = null;
+                    setRoute();
+                    changeState(RobotState.DELIVERING);
+                }
+			} else {
+        		/** The robot is not at the destination yet, move towards it! */
+                current_floor = mode.update(current_floor, destination_floor, false, this);
+			}
+            break;
+	}
+}
+    
+
+    public void changeMode(ModeType modeType) {
+    	if(modeType == ModeType.NORMAL) {
+    		mode = new Normal();
+    	} else if(modeType == ModeType.OVERDRIVE)
+    		mode = new Overdrive(1, 2, 5);
     }
+    
+    
+    public void mailDelivered(boolean overdrive, int weight) {
+//    	
+//    	
+//    	!!!!!!!!!NEEDS TO BE IMPLEMENTED WITH THE STATSTRACKER!!!!!!!!!!!!!
+//    	
+//    	
+    }
+
 
     /**
      * Sets the route for the robot
@@ -152,6 +245,9 @@ public class Robot {
 
 	public MailItem getTube() {
 		return tube;
+	}
+	public MailItem getHand() {
+		return deliveryItem;
 	}
     
 	static private int count = 0;
